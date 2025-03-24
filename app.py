7e9bfd7a0dfaf6a353c8b3839210285ef4740f26
Aditya7005446231 <adityarai77053@gmail.com>
2025-03-24 14:32:27 +0530

    added some extra information about deadlock

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 from flask import Flask, render_template, request
-from model import train_model, predict_deadlock
+from model import predict_deadlock  # Assume model functions are in model.py
 
 app = Flask(__name__)
 
-# Route for home page
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-# Route to handle model training
-@app.route('/train', methods=['POST'])
-def train():
-    train_model()
-    return "Model trained successfully!"
+@app.route('/deadlock')
+def deadlock_info():
+    return render_template('deadlock.html')
 
-# Route to handle deadlock prediction
-@app.route('/predict', methods=['POST'])
-def predict():
-    allocated_resources = int(request.form['allocated_resources'])
-    requested_resources = int(request.form['requested_resources'])
-    
-    # Call prediction function
-    result = predict_deadlock(allocated_resources, requested_resources)
-    
-    if result == 1:
-        return "Deadlock detected!"
-    else:
-        return "No deadlock detected."
+
+
+@app.route('/predictor', methods=['GET', 'POST'])
+def predictor():
+    if request.method == 'POST':
+        allocated_resources = request.form['allocated_resources']
+        requested_resources = request.form['requested_resources']
+        prediction = predict_deadlock(allocated_resources, requested_resources)
+        return render_template('predictor.html', prediction=prediction)
+    return render_template('predictor.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
